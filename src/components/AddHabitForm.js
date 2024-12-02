4e020f5a80253aef34f6f9142bb5aeff4e69efda
23051790-Alexa <23051790@myrp.edu.sg>
2024-12-02 14:14:06 +0800

    Updated

--- src/components/AddHabitForm.js
+++ src/components/AddHabitForm.js
@@ -5,9 +5,23 @@ export default function AddHabitForm({ onAddHabit }) {
 
   const handleSubmit = (e) => {
     // TODO: write code to handle form submission
+    e.preventDefault();
+    if (habitName.trim()) {
+      onAddHabit(habitName);
+      setHabitName("");
+    }
   };
 
-  return {
+  return (
     //TODO: add a form to add a new habit
-  };
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="New Habit"
+        value={habitName}
+        onChange={(e) => setHabitName(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
 }
